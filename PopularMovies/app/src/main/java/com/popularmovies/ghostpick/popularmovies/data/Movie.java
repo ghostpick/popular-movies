17861cc21dc73eff75344795539832e5213141f9
Tiago Galvão <tiagogalvao90@gmail.com>
2017-02-03 08:27:29 +0000

    Movie object added

---
+++ PopularMovies/app/src/main/java/com/popularmovies/ghostpick/popularmovies/data/Movie.java
@@ -0,0 +1,31 @@
+package com.popularmovies.ghostpick.popularmovies.data;
+
+public class Movie {
+
+    private String title;
+    private String overview;
+    private String photo;
+    private String releaseDate;
+    private String voteAverage;
+
+    public String getTitle() {return title;}
+
+    public void setTitle(String title) {this.title = title;}
+
+    public String getOverview() {return overview;}
+
+    public void setOverview(String overview) {this.overview = overview;}
+
+    public String getPhoto() {return photo;}
+
+    public void setPhoto(String photo) {this.photo = photo;}
+
+    public String getReleaseDate() {return releaseDate;}
+
+    public void setReleaseDate(String releaseDate) {this.releaseDate = releaseDate;}
+
+    public String getVoteAverage() {return voteAverage;}
+
+    public void setVoteAverage(String voteAverage) {this.voteAverage = voteAverage;}
+
+}
